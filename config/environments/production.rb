2b334e09f6669fc4ab1e2f038349b7d0e6d604c6
Maximo Miranda <maximomirandah@gmail.com>
2024-08-12 20:02:43 -0500

    feature(app): added new config in docker-entry file

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -72,7 +72,7 @@ Rails.application.configure do
   }
 
   # Use a real queuing backend for Active Job (and separate queues per environment).
-  # config.active_job.queue_adapter = :resque
+  config.active_job.queue_adapter = :good_job
   # config.active_job.queue_name_prefix = "rails_evoluciona057_app_production"
 
   config.action_mailer.perform_caching = false
